b33aa39c1e5a3f2cfe69a161054463bb3636274f
Juan Cruz <juancmarquez90@gmail.com>
2023-12-10 17:12:40 -0300

    Correciones permiso de edad Primera Preentrega 2

--- Primera Preentrega + Márquez Juan Cruz/main.js
+++ Primera Preentrega + Márquez Juan Cruz/main.js
@@ -2,22 +2,30 @@ let nombre = prompt("Ingrese su nombre");
 
 
 if (nombre === "") {
-    alert("¡Debes colocar tu nombre!")
+    alert("¡Debes colocar tu nombre!");
 }
 
 else {
     alert(`¡Bienvenido/a, ${nombre}!`);
 }
 
+
 let edad = prompt("¿Cuál es tu edad?");
 
-while (edad <= 17 || (""))  {
+while (edad <= 17 || ("")) {
     edad = prompt("Para ingresar, debes ser mayor de edad.");
 }
 
 if (edad >= 18) {
     alert("¡Bienvenido/a a Kinkon Bebidas S.A.!");
 }
+
+
+
+
+
+
+
 
 
 
