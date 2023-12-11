bcf93ab79d0422865a2aaab80a4651cb53839378
Juan Cruz <juancmarquez90@gmail.com>
2023-12-11 02:05:45 -0300

    Mejora general del código para la primera preentrega de Coder JS

--- Primera Preentrega + Márquez Juan Cruz/main.js
+++ Primera Preentrega + Márquez Juan Cruz/main.js
@@ -2,30 +2,27 @@ let nombre = prompt("Ingrese su nombre");
 
 
 while (nombre === "") {
-    nombre = prompt ("¡Debes colocar tu nombre!");
+    nombre = prompt("¡Debes colocar tu nombre!");
 }
 
 if (nombre === nombre) {
     alert(`¡Bienvenido/a, ${nombre}!`);
 }
 
-
 let edad = prompt("¿Cuál es tu edad?");
 
 while (edad === "") {
-    edad = prompt ("¡Debes colocar tu edad!");
+    edad = prompt("¡Debes colocar tu edad!");
 }
 
-while (edad >= 120) {
-    edad = prompt ("Debes ingresar una edad válida.")
-}
-
-while (edad <= 17) {
-    edad = prompt("Para ingresar, debes ser mayor de edad.");
-}
-
-if (edad >= 18) {
-    alert("¡Bienvenido/a a Kinkon Bebidas S.A.!");
+if (!isNaN(edad)) {
+    if (edad >= 18 && edad <= 120) {
+        alert("¡Bienvenido/a a Kinkon Bebidas S.A.!");
+    } else {
+        alert("Debes ingresar una edad válida para tener acceso a Kinkon Bebidas S.A.");
+    }
+} else {
+    alert("Debes ingresar un número válido para la edad.");
 }
 
 
