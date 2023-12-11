4cd17be2a5ee6d551ff209b415fe596dc246793f
Juan Cruz <juancmarquez90@gmail.com>
2023-12-11 01:23:28 -0300

    Corrección de ciclos

--- Primera Preentrega + Márquez Juan Cruz/main.js
+++ Primera Preentrega + Márquez Juan Cruz/main.js
@@ -1,18 +1,26 @@
 let nombre = prompt("Ingrese su nombre");
 
 
-if (nombre === "") {
-    alert("¡Debes colocar tu nombre!");
+while (nombre === "") {
+    nombre = prompt ("¡Debes colocar tu nombre!");
 }
 
-else {
+if (nombre === nombre) {
     alert(`¡Bienvenido/a, ${nombre}!`);
 }
 
 
 let edad = prompt("¿Cuál es tu edad?");
 
-while (edad <= 17 || ("")) {
+while (edad === "") {
+    edad = prompt ("¡Debes colocar tu edad!");
+}
+
+while (edad >= 120) {
+    edad = prompt ("Debes ingresar una edad válida.")
+}
+
+while (edad <= 17) {
     edad = prompt("Para ingresar, debes ser mayor de edad.");
 }
 
