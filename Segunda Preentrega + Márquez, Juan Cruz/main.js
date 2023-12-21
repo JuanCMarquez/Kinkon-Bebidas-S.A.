af47088ded0b117ca99d7b83098efd9f38760754
Juan Cruz <juancmarquez90@gmail.com>
2023-12-21 13:06:25 -0300

    Primera esbozo de Segunda Entrega

---
+++ Segunda Preentrega + Márquez, Juan Cruz/main.js
@@ -0,0 +1,58 @@
+let nombre = prompt("Ingrese su nombre");
+
+
+while (nombre === "" || nombre === null) {
+    nombre = prompt("¡Debes colocar tu nombre!");
+}
+
+if (nombre === nombre) {
+    alert(`¡Bienvenido/a, ${nombre}!`);
+}
+
+let edad = prompt("¿Cuál es tu edad?");
+
+while (edad === "" || edad === null) {
+    edad = prompt("¡Debes colocar tu edad!");
+}
+
+if (!isNaN(edad)) {
+    if (edad >= 18 && edad <= 120) {
+        alert("¡Bienvenido/a a Kinkon Bebidas S.A.!");
+    } else {
+        alert("Debes ingresar una edad válida para tener acceso a Kinkon Bebidas S.A.");
+    }
+} else {
+    alert("Debes ingresar un número válido para la edad.");
+}
+
+let bebidasAlcoholicas = [
+    {
+        nombre: "Cerveza",
+        tipo: "Lager",
+        porcentajeAlcohol: 5.0,
+        precio: 2.99
+    },
+    {
+        nombre: "Vino tinto",
+        tipo: "Cabernet Sauvignon",
+        porcentajeAlcohol: 13.5,
+        precio: 14.99
+    },
+    {
+        nombre: "Whisky",
+        tipo: "Single Malt",
+        porcentajeAlcohol: 40.0,
+        precio: 49.99
+    },
+
+];
+
+function obtenerPropiedades(arr, propiedad) {
+    return arr.map(function (bebida) {
+        return bebida[propiedad];
+    });
+}
+
+let nombresDeBebidas = bebidasAlcoholicas.map(bebida => bebida.nombre);
+alert (`Estos son los nombres de las bebidas: ${nombresDeBebidas}`);
+
