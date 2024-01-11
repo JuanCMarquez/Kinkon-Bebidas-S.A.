fffcccab1abf795c8bfe5da78367d89d519bb6f3
Juan Cruz <juancmarquez90@gmail.com>
2024-01-11 02:40:55 -0300

    Agrego Carrousel y corrijo css

--- Tercera Preentrega + Márquez/inicio.js
+++ Tercera Preentrega + Márquez/inicio.js
@@ -5,25 +5,6 @@ function playAudio() {
 
 // Función para confirmar la edad
 function confirmAge() {
-    const isAdult = confirm("¿Eres mayor de edad?");
-
-    if (isAdult) {
-        // Guardar en local storage que el usuario confirmó ser mayor de edad
-        localStorage.setItem("isAdult", "true");
-
-        // Ocultar el mensaje de verificación de edad
-        document.getElementById("age-verification").style.display = "none";
-
-        // Mostrar el menú de navegación
-        document.getElementById("main-nav").style.display = "block";
-    } else {
-        // Redirigir o mostrar un mensaje indicando que no se permite el acceso
-        alert("Lo siento, debes ser mayor de edad para acceder a esta tienda.");
-        window.location.href = "https://www.google.com"; // Redirigir a Google como ejemplo
-    }
-}
-
-function confirmAge() {
     // Reproducir audio
     playAudio();
 
@@ -32,11 +13,14 @@ function confirmAge() {
     ageVerificationSection.style.display = 'none';
 
     // Mostrar la barra de navegación y el contenido verificado
-    var navBar = document.getElementById('main-nav');
+    var navCarouselContainer = document.getElementById('nav-carousel-container');
     var verifiedContent = document.getElementById('age-verified-content');
 
-    navBar.style.display = 'block';
+    navCarouselContainer.style.display = 'block';
     verifiedContent.style.display = 'block';
+
+    // Guardar en local storage que el usuario confirmó ser mayor de edad
+    localStorage.setItem("isAdult", "true");
 }
 
 // Función para cargar la página de inicio
@@ -59,11 +43,12 @@ window.onload = function () {
     const isAdult = localStorage.getItem("isAdult");
 
     if (isAdult === "true") {
-        // Usuario ya confirmó ser mayor de edad, cargar la página de inicio
         loadHome();
-        document.getElementById("main-nav").style.display = "block";
+        document.getElementById("nav-carousel-container").style.display = "block";
     } else {
-        // Usuario no ha confirmado ser mayor de edad, mostrar el mensaje de verificación
         document.getElementById("age-verification").style.display = "block";
     }
 };
+
+
+
